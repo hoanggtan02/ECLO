eb974196a3184865ec3dbaf2f23d8a5018003422
ducnguyen11101 <168653353+ducnguyen11101@users.noreply.github.com>
2025-03-26 14:09:26 +0700

    pb

--- controllers/core/staffConfiguration.php
+++ controllers/core/staffConfiguration.php
@@ -4,10 +4,11 @@ if (!defined('ECLO')) die("Hacking attempt");
 $jatbi = new Jatbi($app);
 $setting = $app->getValueData('setting');
 
-$app->router("/staffConfiguration", 'GET', function($vars) use ($app, $jatbi, $setting) {
+$app->router("/staffConfiguration/department", 'GET', function($vars) use ($app, $jatbi, $setting) {
     $vars['title'] = $jatbi->lang("Cấu hình nhân sự");
+    $vars['title1'] = $jatbi->lang("Phòng ban");
     $vars['employee'] = $app->select("employee",["name (text)","sn (value)"],[]);
-    echo $app->render('templates/staffConfiguration/menuDepartment.html', $vars);
+    echo $app->render('templates/staffConfiguration/department.html', $vars);
 })->setPermissions(['staffConfiguration']);
 
 ?>
